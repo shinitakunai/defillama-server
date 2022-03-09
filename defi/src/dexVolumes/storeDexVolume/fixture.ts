b9741d02e0dfd8ca2e6980c2525bc29f97b4ff0c
shinitakunai <87251045+shinitakunai@users.noreply.github.com>
2022-03-09 02:14:42 -0800

    add dexMeta fixtures

---
+++ defi/src/dexVolumes/storeDexVolume/fixture.ts
@@ -0,0 +1,9 @@
+import { DexVolumeMetaRecord } from "../dexVolume.types";
+
+export const dexMeta: DexVolumeMetaRecord[] = [
+  { id: 468, module: "traderjoe", name: "Trader Joe", locked: false },
+  { id: 302, module: "spookyswap", name: "SpookySwap", locked: true },
+  { id: 119, module: "sushiswap", name: "SushiSwap", locked: true },
+  { id: 1, module: "uniswap", name: "Uniswap", locked: false },
+  { id: 311, module: "spiritswap", name: "SpiritSwap", locked: true },
+];
